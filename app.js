90ac06487dfda4f5fbd975bf3abe82c4021e54d8
Luiz-Augusto-06 <79378201+Luiz-Augusto-06@users.noreply.github.com>
2022-06-13 09:56:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './componentes/Header'
+import Home from './componentes/Home'
+
+
+
+export default function App (props) {
+
+  return (
+    <div>
+       <Header/>
+       <Home/>
+       
+    </div>
+
+  )
+}
+
---
+++ index.js
@@ -0,0 +1,13 @@
+//import de arquivos
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+
+ReactDOM.render(
+  <div className=''> 
+    <App/>
+  </div>
+  //A class onde ira manda 
+  ,document.getElementById('react-app')
+);
